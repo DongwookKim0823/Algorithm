5a0bd9eabd7d68115ae3fffd9f16b39ec0643537
DongwookKim0823 <dwkim0823@naver.com>
2024-10-18 19:32:16 +0900

    Create 조건에 맞는 개발자 찾기.sql

---
+++ 프로그래머스/SQL/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,13 @@
+-- 코드를 작성해주세요
+SELECT
+    DISTINCT ID,
+    EMAIL,
+    FIRST_NAME,
+    LAST_NAME
+FROM
+    DEVELOPERS,
+    SKILLCODES
+WHERE
+    (NAME = 'Python' or NAME = 'C#') AND (SKILL_CODE & CODE) > 0
+ORDER BY
+    ID

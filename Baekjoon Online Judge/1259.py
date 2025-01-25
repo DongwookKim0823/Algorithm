23e65c890703c61c567dae90aa6977cb5adee794
DongwookKim0823 <dwkim0823@naver.com>
2025-01-25 14:44:45 +0900

    Create 1259.py

---
+++ Baekjoon Online Judge/1259.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    
+    while True:
+        string = input()
+        if string == '0':
+            break
+        
+        if string == string[::-1]:
+            print('yes')
+        else:
+            print('no')
